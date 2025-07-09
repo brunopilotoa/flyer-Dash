17460a14847f0428cbb92a1687a948be2f7c0888
brunopilotoa <52364626+brunopilotoa@users.noreply.github.com>
2025-07-09 07:09:37 -0300

    Update 1_NC_H23.py
    Invertido TAB 1 e 2 
    Removido Grafico da parte por aviao

--- pages/1_NC_H23.py
+++ pages/1_NC_H23.py
@@ -3,12 +3,41 @@ from Inicio import panes, px, base
 
 st.title("Visao Geral NCs H23")
 tab1, tab2 = st.tabs(
-    [
-        "Graficos NC",
+    [    
         "Por Aviao",
+        "Graficos NC"
+        
     ]
 )
+
 with tab1:
+    # Cria o selectbox
+    prefixo = st.selectbox(
+        "",
+        panes["Prefixo"].unique(),
+        index=None,
+        placeholder="Selecione a Aeronave",
+    )
+    col1, col2 = st.columns(2, gap="medium")
+    with col1:
+        st.subheader("NC em Aberto")
+        # Filtra o dataset pelo selectbox
+        panes_filtered = panes[panes["Prefixo"] == prefixo]
+        opened_pane = panes_filtered[panes_filtered["Status"] != "Solucionado"]
+        st.dataframe(opened_pane["Descrição da Não Conformidade"])
+    with col2:
+        st.subheader("NC Concluidas")
+        closed_pane = panes_filtered[panes_filtered["Status"] == "Solucionado"]
+        closed_pane["Descrição da Não Conformidade"]
+    #with col3:
+        # Cria o Grafico
+        #sector_panes = (
+            #px.bar(panes_filtered, x="Class NC", title="Quantidade Por Setor")
+            #.update_xaxes(categoryorder="total descending", title="Setor")
+            #.update_yaxes(title="Quantia")
+        #)
+        #st.plotly_chart(sector_panes)
+with tab2:
     (col1, col2, col3, col4) = st.columns(4)
     with col1:
         # Figura que Mostra a Quantidade de Items por Setor AVI,GMP,CEL
@@ -60,30 +89,3 @@ with tab1:
             .update_xaxes(title="Prefixo")
         )
         st.plotly_chart(fig3, y="Prefixo")
-with tab2:
-    # Cria o selectbox
-    prefixo = st.selectbox(
-        "",
-        panes["Prefixo"].unique(),
-        index=None,
-        placeholder="Selecione a Aeronave",
-    )
-    col1, col2, col3 = st.columns(3, gap="small")
-    with col1:
-        st.subheader("NC em Aberto")
-        # Filtra o dataset pelo selectbox
-        panes_filtered = panes[panes["Prefixo"] == prefixo]
-        opened_pane = panes_filtered[panes_filtered["Status"] != "Solucionado"]
-        st.dataframe(opened_pane["Descrição da Não Conformidade"])
-    with col2:
-        st.subheader("NC Concluidas")
-        closed_pane = panes_filtered[panes_filtered["Status"] == "Solucionado"]
-        closed_pane["Descrição da Não Conformidade"]
-    with col3:
-        # Cria o Grafico
-        sector_panes = (
-            px.bar(panes_filtered, x="Class NC", title="Quantidade Por Setor")
-            .update_xaxes(categoryorder="total descending", title="Setor")
-            .update_yaxes(title="Quantia")
-        )
-        st.plotly_chart(sector_panes)
